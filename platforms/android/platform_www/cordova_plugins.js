9f1c836ea1d5cad31ad078ea4423a4c65791e3ec
Tushar Vengurlekar <tushar.vengurlekar@bidchat.com>
2016-07-11 16:16:09 +0530

    Added oAuth Platform

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -7,13 +7,21 @@ module.exports = [
             "cordova.InAppBrowser.open",
             "window.open"
         ]
+    },
+    {
+        "file": "plugins/com.oauthio.plugins.oauthio/dist/oauth.js",
+        "id": "com.oauthio.plugins.oauthio.OAuth",
+        "merges": [
+            "OAuth"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.2",
-    "cordova-plugin-inappbrowser": "1.3.0"
+    "cordova-plugin-inappbrowser": "1.3.0",
+    "com.oauthio.plugins.oauthio": "0.2.4"
 };
 // BOTTOM OF METADATA
 });
--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -8,13 +8,22 @@ module.exports = [
             "cordova.InAppBrowser.open",
             "window.open"
         ]
+    },
+    {
+        "file": "plugins/com.oauthio.plugins.oauthio/dist/oauth.js",
+        "id": "com.oauthio.plugins.oauthio.OAuth",
+        "pluginId": "com.oauthio.plugins.oauthio",
+        "merges": [
+            "OAuth"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.2",
-    "cordova-plugin-inappbrowser": "1.3.0"
+    "cordova-plugin-inappbrowser": "1.3.0",
+    "com.oauthio.plugins.oauthio": "0.2.4"
 }
 // BOTTOM OF METADATA
 });
