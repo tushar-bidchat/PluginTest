8eb6be52d879cb45b869e58d968f58ff11934cb3
Tushar Vengurlekar <tushar.vengurlekar@bidchat.com>
2016-07-12 13:09:50 +0530

    Add Tests for cordova-plugin-geolocation

--- www/js/index.js
+++ www/js/index.js
@@ -25,7 +25,9 @@ function buttonOneClicked() {
     // getDeviceInfo();
     // getDeviceOrientation();
     
-    showInformationMessage('Button Clicked Natively !');
+    // showInformationMessage('Button Clicked Natively !');
+    
+    showCurrentGeoLocation();
 }
 
 function showInformationMessage(message) {
@@ -42,6 +44,32 @@ function showInformationMessage(message) {
     );
 }
 
+function showCurrentGeoLocation () {
+    
+    // onSuccess Callback
+    // This method accepts a Position object, which contains the
+    // current GPS coordinates
+    //
+    var onSuccess = function(position) {
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
 /**
  * Default Implemenetion of Capture Picture from Camera Gallery
  */      
